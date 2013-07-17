549146a9b3bcd7447f8c73f614767140471b67e7
Wei Hsu <yayitswei@gmail.com>
2013-07-17 01:17:45 -0700

    formatting

--- src/maestro/core.clj
+++ src/maestro/core.clj
@@ -5,29 +5,29 @@
 ;;(System/setProperty "gnu.io.rxtx.SerialPorts" "/dev/ttyACM0")
 
 (defsynth dubstep [bpm 120 wobble 1 note 50 snare-vol 1 kick-vol 1 v 1]
- (let [trig (impulse:kr (/ bpm 120))
-       freq (midicps note)
-       swr (demand trig 0 (dseq [wobble] INF))
-       sweep (lin-exp (lf-tri swr) -1 1 40 3000)
-       wob (apply + (saw (* freq [0.99 1.01])))
-       wob (lpf wob sweep)
-       wob (* 0.8 (normalizer wob))
-       wob (+ wob (bpf wob 1500 2))
-       wob (+ wob (* 0.2 (g-verb wob 9 0.7 0.7)))
+  (let [trig (impulse:kr (/ bpm 120))
+        freq (midicps note)
+        swr (demand trig 0 (dseq [wobble] INF))
+        sweep (lin-exp (lf-tri swr) -1 1 40 3000)
+        wob (apply + (saw (* freq [0.99 1.01])))
+        wob (lpf wob sweep)
+        wob (* 0.8 (normalizer wob))
+        wob (+ wob (bpf wob 1500 2))
+        wob (+ wob (* 0.2 (g-verb wob 9 0.7 0.7)))
 
-       kickenv (decay (t2a (demand (impulse:kr (/ bpm 30)) 0 (dseq [1 0 0 0 0 0 1 0 1 0 0 1 0 0 0 0] INF))) 0.7)
-       kick (* (* kickenv 7) (sin-osc (+ 40 (* kickenv kickenv kickenv 200))))
-       kick (clip2 kick 1)
+        kickenv (decay (t2a (demand (impulse:kr (/ bpm 30)) 0 (dseq [1 0 0 0 0 0 1 0 1 0 0 1 0 0 0 0] INF))) 0.7)
+        kick (* (* kickenv 7) (sin-osc (+ 40 (* kickenv kickenv kickenv 200))))
+        kick (clip2 kick 1)
 
-       snare (* 3 (pink-noise [1 1]) (apply + (* (decay (impulse (/ bpm 240) 0.5) [0.4 2]) [1 0.05])))
-       snare (+ snare (bpf (* 4 snare) 2000))
-       snare (clip2 snare 1)]
+        snare (* 3 (pink-noise [1 1]) (apply + (* (decay (impulse (/ bpm 240) 0.5) [0.4 2]) [1 0.05])))
+        snare (+ snare (bpf (* 4 snare) 2000))
+        snare (clip2 snare 1)]
 
-   (out 0 (* v (clip2 (+ wob (* kick-vol kick) (* snare-vol snare)) 1)))))
+    (out 0 (* v (clip2 (+ wob (* kick-vol kick) (* snare-vol snare)) 1)))))
 
 (defsynth monotron
   "Korg Monotron from website diagram:
-   http://korg.com/services/products/monotron/monotron_Block_diagram.jpg."
+  http://korg.com/services/products/monotron/monotron_Block_diagram.jpg."
   [note     60            ; midi note value
    volume   0.7           ; gain of the output
    mod_pitch_not_cutoff 1 ; use 0 or 1 only to select LFO pitch or cutoff modification
@@ -60,15 +60,12 @@
 (defn modify-monotron [a]
   (ctl d :pitch a))
 
-(defn start!
-  []
+(defn start! [portname]
   ;;(def d (monotron 40 0.8 1 0.0 2.5 350.0 800.0 3.0))
   (def d (dubstep))
   (def port (open portname 9600))
   (on-byte port modify-dubstep))
 
-(defn stop!
-  []
+(defn stop! []
   (stop)
-  (close port)
-)
+  (close port))
